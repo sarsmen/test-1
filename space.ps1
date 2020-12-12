a7b1d6ddbf3c893d16c85d1d285a463772f1dd1d
sarsmen <42042380+sarsmen@users.noreply.github.com>
2020-12-12 16:04:13 +0700

    Update space.ps1

--- space.ps1
+++ space.ps1
@@ -1,12 +1,6 @@
 function WriteMessage()
 {
-[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-$Url = "https://the.earth.li/~sgtatham/putty/latest/w32/putty.exe"
-$Path = "$env:TEMP\\putty.exe"
-$webClient = new-object System.Net.WebClient
-$webClient.DownloadFile( $Url, $Path )
-Start-Process($Path); 
-exit;
+    Write-Host "This is a test PowerShell Script that does no harm at all" -ForegroundColor Green -BackgroundColor DarkGray
 }
 
 WriteMessage
